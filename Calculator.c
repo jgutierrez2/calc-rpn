d8303caa73e9175b743b4778987b9bf12a26adc6
jgutierrez2 <jacob.gutierrez28@gmail.com>
2015-05-11 13:36:59 -0500

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include "Calculator.h"
+#include "Stack.h"
+#include "Storage.h"
 
 struct Calculator {
+  Stack stack;
+  Storage mem;
+}
 
+Calculator *newCalculator(){
+  Calculator *c = malloc(sizeof(Calculator));
+  c->stack = newStack();
+  c->mem = newStorage();
+}
+
+void destroyCalculator(Calculator *c){
+  destroyStack(c->stack);
+  destroyStorage(c->Storage);
+  free(c);
+}
+
+void input(Calculator *c, int i){
+  push(c->stack,i);
+}
+
+void compute(Calculator *c, char *sym){
+  
 }
