037f4c7c319d1d34928dbea3209d6599242101c6
jgutierrez2 <jacob.gutierrez28@gmail.com>
2015-05-15 12:30:21 -0500

    Update Stack.h

--- Calculator/Calculator/Stack.h
+++ Calculator/Calculator/Stack.h
@@ -3,6 +3,6 @@ typedef struct Stack Stack;
 Stack *newstack();
 void destroystack();
 int size(Stack *stk);
-int pop(Stack *stk);
+int pop(Stack* stk, double* out);
 void push(Stack *stk, int y);
-int at(Stack *stk, int i);
+int at(Stack *stk, int i, double* out);
