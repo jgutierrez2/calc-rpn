076821471fbbb2a5c2f6318c82bd16634f41e257
jgutierrez2 <jacob.gutierrez28@gmail.com>
2015-05-15 12:29:14 -0500

    Update Stack.c

--- Calculator/Calculator/Stack.c
+++ Calculator/Calculator/Stack.c
@@ -4,13 +4,19 @@
 #define STACKSIZE 100
 
 struct Stack {
-  int xs[STACKSIZE];
+  double xs[STACKSIZE];
   int size;
 };
 
 Stack *newstack(){
   Stack *s = malloc(sizeof(Stack));
   s->size = 0;
+  
+  int i;
+  for (i = 0; i < STACKSIZE; i++) {
+    s->xs[i] = 0;
+  }
+  
   return s;
 }
 
@@ -22,17 +28,24 @@ int size(Stack *stk) {
   return stk->size;
 }
 
-int pop(Stack *stk){
-  stk->size--;
-  int r = stk->xs[stk->size];
-  return r;
+int pop(Stack *stk, double *out){
+  if (stk->size > 0) {
+    stk->size--;
+    *out = stk->xs[stk->size];
+    return 1;
+  }
+  return 0;
 }
 
-void push(Stack *stk, int y) {
+void push(Stack *stk, double y) {
   stk->xs[stk->size] = y;
   stk->size++;
 }
 
-int at(Stack *stk, int i) {
-  return stk->xs[i];
+int at(Stack *stk, int i, double* out) {
+  if (i < stk->size) {
+    *out = stk->xs[i]
+    return 1;
+  }
+  return 0;
 }
